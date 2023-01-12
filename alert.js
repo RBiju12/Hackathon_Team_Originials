a1f1e14a456f06dfa3237309c155beae24412b37
Rishan Biju <rishanbiju@gmail.com>
2023-01-12 23:49:29 +0000

    Initial commit

---
+++ alert.js
@@ -0,0 +1,8 @@
+function alerts(num){
+  for(let i = 0; i < 1; i++){
+    alert("Welcome to Environmental Solutions");
+    num = i;
+  }
+}
+
+alerts(0);
---
+++ script.js
@@ -0,0 +1,22 @@
+/**class something_lol{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+  hi(){
+    
+  }
+}
+yolo = new something_lol(20,30);
+yolo.hi();**/
+function toProb(){
+  window.location.href = "problem.html"
+}
+function toSol(){
+    window.location.href = "methods.html"
+}
+function toHome(){
+    window.location.href = "index.html"
+}
+
+
